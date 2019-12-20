72139b74c0d8e45a8ce76fef7fb54107ff798edb
Asuk Kono <asukiaaa@gmail.com>
2019-12-20 12:59:29 +0900

    Remove reundand comments on startSession

--- src/startSession.py
+++ src/startSession.py
@@ -1,16 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
-url = 'http://localhost:4444/wd/hub'
-# print(url)
-# print(webdriver.DesiredCapabilities.FIREFOX)
+url = 'https://www.quaxio.com/2048/'
+executor_url = 'http://localhost:4444/wd/hub'
+capabilities = webdriver.DesiredCapabilities.FIREFOX
 
-# browser = webdriver.Remote(command_executor=url,desired_capabilities={})
-browser = webdriver.Remote(command_executor=url, desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
+browser = webdriver.Remote(command_executor=executor_url, desired_capabilities=capabilities)
+browser.get(url)
 
-# browser = webdriver.Firefox()
-
-browser.get('https://www.quaxio.com/2048/')
 print(browser.session_id)
-
